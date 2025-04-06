dba411c8d1ca4a0f855fc4ca93c72d24853a19d8
Lewin <lewinelep@gmail.com>
2025-04-06 14:47:40 -0700

    edited submission.cs

--- submission.cs
+++ submission.cs
@@ -40,18 +40,110 @@ namespace ConsoleApp1
         // Q2.1
         public static string Verification(string xmlUrl, string xsdUrl)
         {
+            try
+            {
+                XmlSchemaSet schemas = new XmlSchemaSet();
+                schemas.Add("", xsdUrl);
 
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas = schemas;
+                settings.ValidationType = ValidationType.Schema;
 
-            //return "No Error" if XML is valid. Otherwise, return the desired exception message.
+                System.Text.StringBuilder errors = new System.Text.StringBuilder();
+                settings.ValidationEventHandler += (sender, args) =>
+                {
+                    errors.AppendLine(args.Message);
+                };
+
+                using (XmlReader reader = XmlReader.Create(xmlUrl, settings))
+                {
+                    while (reader.Read()) { }
+                }
+
+                if (errors.Length == 0)
+                {
+                    return "No Error";
+                }
+                else
+                {
+                    return errors.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Exception: " + ex.Message;
+            }
         }
 
         public static string Xml2Json(string xmlUrl)
         {
-            
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(xmlUrl);
 
-            // The returned jsonText needs to be de-serializable by Newtonsoft.Json package. (JsonConvert.DeserializeXmlNode(jsonText))
-            return jsonText;
+                var hotelList = new List<dynamic>();
 
+                XmlNodeList hotelNodes = doc.SelectNodes("//Hotel");
+                if (hotelNodes == null) return "{}"; 
+
+                foreach (XmlNode hotelNode in hotelNodes)
+                {
+                    dynamic hotelObj = new ExpandoObject();
+                    var hotelDict = (IDictionary<string, object>)hotelObj;
+
+                    var nameNode = hotelNode.SelectSingleNode("Name");
+                    if (nameNode != null)
+                        hotelDict["Name"] = nameNode.InnerText;
+
+                    var phoneNodes = hotelNode.SelectNodes("Phone");
+                    var phoneList = new List<string>();
+                    foreach (XmlNode p in phoneNodes)
+                    {
+                        phoneList.Add(p.InnerText);
+                    }
+                    hotelDict["Phone"] = phoneList;
+
+                    var addressNode = hotelNode.SelectSingleNode("Address");
+                    dynamic addressObj = new ExpandoObject();
+                    var addressDict = (IDictionary<string, object>)addressObj;
+
+                    if (addressNode != null)
+                    {
+                        addressDict["Number"] = addressNode["Number"]?.InnerText;
+                        addressDict["Street"] = addressNode["Street"]?.InnerText;
+                        addressDict["City"]   = addressNode["City"]?.InnerText;
+                        addressDict["State"]  = addressNode["State"]?.InnerText;
+                        addressDict["Zip"]    = addressNode["Zip"]?.InnerText;
+
+                        var nearestAirportAttr = addressNode.Attributes?["NearestAirport"];
+                        if (nearestAirportAttr != null)
+                        {
+                            addressDict["_NearestAirport"] = nearestAirportAttr.Value;
+                        }
+                    }
+                    hotelDict["Address"] = addressObj;
+
+                    var ratingAttr = hotelNode.Attributes?["Rating"];
+                    if (ratingAttr != null)
+                    {
+                        hotelDict["_Rating"] = ratingAttr.Value;
+                    }
+
+                    hotelList.Add(hotelObj);
+                }
+
+                dynamic rootObj = new ExpandoObject();
+                rootObj.Hotels = new ExpandoObject();
+                ((IDictionary<string, object>)rootObj.Hotels)["Hotel"] = hotelList;
+
+                string jsonText = JsonConvert.SerializeObject(rootObj, Formatting.Indented);
+                return jsonText;
+            }
+            catch (Exception ex)
+            {
+                return $"Exception: {ex.Message}";
+            }
         }
     }
 
